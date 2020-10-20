076287fffa0563f654d62870f54b0f156604c8a3
banksu <67863190+banksu@users.noreply.github.com>
2020-10-20 13:09:00 +0900

    https://github.com/banksu/movie_app_twice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ fav, add, papapa }) {
+  return (
+    <div>
+      <h1>{fav}</h1>
+      <h3>{add}</h3>
+      <h3>{papapa}</h3>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Food fav="3434" add="adasdasd" papapa="popopop" />
     </div>
   );
 }
