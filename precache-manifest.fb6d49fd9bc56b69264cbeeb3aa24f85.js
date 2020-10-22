b54dd63c105575267294697f8c0068c594015c06
banksu <67863190+banksu@users.noreply.github.com>
2020-10-22 18:20:48 +0900

    Updates

---
+++ precache-manifest.fb6d49fd9bc56b69264cbeeb3aa24f85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2423494fb0d7fc019ecd424de49720c0",
+    "url": "/movie_app_twice/index.html"
+  },
+  {
+    "revision": "2de4e20e6de07935d4ac",
+    "url": "/movie_app_twice/static/js/2.6fcba606.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app_twice/static/js/2.6fcba606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d07b8ee6d18b35e14f2",
+    "url": "/movie_app_twice/static/js/main.3c59c852.chunk.js"
+  },
+  {
+    "revision": "a1d399a0ec7de46eb5df",
+    "url": "/movie_app_twice/static/js/runtime-main.a8c32213.js"
+  }
+]);
