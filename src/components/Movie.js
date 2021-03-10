af2cf5235a8adf46aabaa02a822a1f34e72db08f
banksu <banksu.cai@gmail.com>
2021-03-10 20:01:22 +0900

    republish

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -22,13 +22,6 @@ function Movie({ id, title, year, genres, summary, poster }) {
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
-          <ul className="movie__genres">
-            {genres.map((genre, index) => (
-              <li key={index} className="genres__genre">
-                {genre}
-              </li>
-            ))}
-          </ul>
           <p className="movie__summary">{summary.slice(0, 140)}...</p>
         </div>
       </Link>
